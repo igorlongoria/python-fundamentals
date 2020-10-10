ac1aa6ed67848e5e18af2a81d0dfec0f3a8ac83a
Igor Longoria <igorlongoria@gmail.com>
2020-10-10 16:41:05 -0500

    Python Fundamentals Lab Completed

--- 01_python_fundamentals/01_01_run_it.py
+++ 01_python_fundamentals/01_01_run_it.py
@@ -6,7 +6,9 @@
 3 - Explore the interpreter.
 	- Execute lines with syntax error and see what the response is.
         * What happens if you leave out a quotation or parentheses?
+        We get a syntax error.
         * How helpful are the error messages?
+Very helpful as it identifies the problem to be solved
 	- Use the help() function to explore what you can do with the interpreter.
         For example execute help('print').
         press q to exit.
@@ -14,3 +16,14 @@
 	- Calculate how many seconds are in a year.
 
 '''
+print("Hello world")
+help('print')
+print(2+2)
+# Seconds in a year formula below
+days = 365.25
+hours_in_a_day = 24
+minutes_in_an_hour = 60
+second_in_a_minute = 60
+seconds_in_a_year = (days * hours_in_a_day * minutes_in_an_hour * second_in_a_minute)
+z = seconds_in_a_year
+print(z)
