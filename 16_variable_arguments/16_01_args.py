99df86d6e466297b20e8b3cd49ff4a27540f0dea
Igor Longoria <igorlongoria@gmail.com>
2020-11-19 14:59:41 -0600

    Variable Arguments Lab Done

--- 16_variable_arguments/16_01_args.py
+++ 16_variable_arguments/16_01_args.py
@@ -2,3 +2,8 @@
 Write a script with a function that demonstrates the use of *args.
 
 '''
+
+def baseball_greatest(*args):
+    for i in args:
+        print(i)
+baseball_greatest("Babe Ruth", "David Ortiz", "Ricky Henderson", "Fernando Valenzuela")
--- 16_variable_arguments/16_02_kwargs.py
+++ 16_variable_arguments/16_02_kwargs.py
@@ -2,3 +2,10 @@
 Write a script with a function that demonstrates the use of **kwargs.
 
 '''
+
+def baseball_positions(**kwargs):
+    for i, p in kwargs.items():
+        print(i, p)
+
+baseball_positions(RF = "Right Field", DH = "Designated Hitter", OF = "Outfielder", P = "Pitcher" )
+
